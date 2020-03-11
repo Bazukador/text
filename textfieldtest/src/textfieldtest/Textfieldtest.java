dc8cd29a46f48d64400da6e0803992c7a6bb20c0
Bazukador <62078969+Bazukador@users.noreply.github.com>
2020-03-11 18:45:14 -0400

    Add files via upload

---
+++ textfieldtest/src/textfieldtest/Textfieldtest.java
@@ -0,0 +1,25 @@
+package textfieldtest;
+
+import javax.swing.JFrame;
+
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Textfieldtest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        textfieldframe textFieldFrame = new textfieldframe();
+        textFieldFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        textFieldFrame.setSize( 1350,1100 );
+        textFieldFrame.setVisible( true );
+         
+    }
+    
+}
